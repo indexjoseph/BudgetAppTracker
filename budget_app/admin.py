0b77eb53308738055ce48680883ed19c8908ad7d
Joseph Oladeji <73621296+indexjoseph@users.noreply.github.com>
2022-09-06 11:36:25 -0400

    Update admin.py

--- budget_app/admin.py
+++ budget_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from budget_app.models import Transaction, Entry
 
-admin.site.register(Transaction)
-admin.site.register(Entry)
+admin.site.register(Transaction) # Register the Transaction model to the Django adminstration site
+admin.site.register(Entry) # Register the Entry model to the Django adminstration site
 # Register your models here.
